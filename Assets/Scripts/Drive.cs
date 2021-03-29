5c02d00d54d41d6fb7b8a60331d43393906f6d5d
Pedro Braga <phbm.braga@gmail.com>
2021-03-29 10:51:40 -0300

    Dot product

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -18,6 +18,8 @@ public class Drive : MonoBehaviour
         direction = fuel.transform.position - transform.position;
         Coords dirNormal = HolisticMath.GetNormal(new Coords(direction));
         direction = dirNormal.ToVector();
+        float angle = HolisticMath.Angle(new Coords(0, 1, 0), new Coords(direction)) * 180.0f/Mathf.PI;
+        Debug.Log("Angle to fuel: " + angle);
     }
 
     void Update()
--- Assets/Scripts/HolisticMath.cs
+++ Assets/Scripts/HolisticMath.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class HolisticMath 
 {
-    static public Coords GetNormal(Coords vector)
+    static public float Square(float value)
     {
-        float distance = Distance(new Coords(0, 0, 0), vector);
-        vector.x /= distance;
-        vector.y /= distance;
-        vector.z /= distance;
-
-        return vector;  
+        return value * value;
     }
+
 
     static public float Distance(Coords point1, Coords point2)
     {
@@ -23,8 +19,33 @@ public class HolisticMath
         return Mathf.Sqrt(Square(x) + Square(y) + Square(z));
     }
 
-    static public float Square(float value)
+    static public Coords GetNormal(Coords vector)
     {
-        return value * value;
+        float distance = Distance(new Coords(0, 0, 0), vector);
+        vector.x /= distance;
+        vector.y /= distance;
+        vector.z /= distance;
+
+        return vector;  
+    }
+
+    static public float Dot(Coords vector1, Coords vector2)
+    {
+        // Dot(vector1, vector2) > 0       angle < 90
+        // Dot(vector1, vector2) = 0       angle = 90
+        // Dot(vector1, vector2) < 0       angle > 90
+
+        return vector1.x * vector2.x + vector1.y * vector2.y + vector1.z * vector1.z;
+    }
+
+    static public float Angle(Coords vector1, Coords vector2)
+    {
+        // Independent to order 
+        float denominator = Dot(vector1, vector2);
+        // Distance is always positive because of pitagoras
+        // Also independent to order
+        float numerator = Distance(new Coords(0, 0, 0), vector1) * Distance(new Coords(0, 0, 0), vector2);
+
+        return Mathf.Acos(denominator / numerator); // radians. For degree * 180/Mathf.PI;
     }
 }
